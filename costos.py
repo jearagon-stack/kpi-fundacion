1b12003a7125ee03e80680057680219f843f8088
Ernesto Aragon <jearagon@fundagondra.sv>
2026-04-17 07:26:42 -0600

    fix: Correccion del radio button y ruteo de Libreria

--- costos.py
+++ costos.py
@@ -3,13 +3,13 @@ import streamlit as st
 # Importamos las funciones principales de nuestros nuevos archivos
 from costos_cafeteria import mostrar_modulo_costos as modulo_cafeteria
 from costos_talleres import mostrar_modulo_costos as modulo_talleres
-from costs_library import mostrar_modulo_libreria as modulo_libreria # <--- Importación de la Librería
+from costs_library import mostrar_modulo_libreria as modulo_libreria
 
 def mostrar_modulo_costos():
-    # 1. El Menú de Decisión
+    # 1. El Menú de Decisión (AQUÍ ESTÁN LOS 3 BOTONES)
     unidad = st.radio(
         "🎯 Selecciona la Unidad de Negocio a evaluar:",
-        ["☕ Cafetería y Despensa", "🖨️ Talleres Gráficos", "📚 Librería"], # <--- Agregamos Librería al menú
+        ["☕ Cafetería y Despensa", "🖨️ Talleres Gráficos", "📚 Librería"],
         horizontal=True
     )
     
@@ -20,6 +20,5 @@ def mostrar_modulo_costos():
         modulo_cafeteria()
     elif unidad == "🖨️ Talleres Gráficos":
         modulo_talleres()
-    elif unidad == "📚 Librería": # <--- Desvío hacia la nueva sección
+    elif unidad == "📚 Librería":
         modulo_libreria()
-        # Truco para forzar actualizacion de cache
